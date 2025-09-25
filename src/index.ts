529e4f401824c4277bfb9c5eed79a687aa1f37c2
TAGUSA Kentaro <tagusa-k@grassfield.online>
2025-09-26 00:36:47 +0900

    Web サーバ化

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,48 @@
-import { read } from 'node-dht-sensor';
+import express, { type Request, type Response } from "express";
+import { read } from "node-dht-sensor";
 
 const SENSOR_TYPE = 22;
 const GPIO_PIN = 2;
-const INTERVAL = 1000;
 
-console.log('datetime,temperature,humidity');
-setInterval(readSensor, INTERVAL);
-
-function readSensor() {
+function getSensorData() {
   const result = read(SENSOR_TYPE, GPIO_PIN);
 
-  const now = formatDate(new Date());
-  const temperature = result.isValid ? result.temperature.toFixed(1) : '[error]';
-  const humidity = result.isValid ? result.humidity.toFixed(1) : '[error]';
+  const now = new Date();
+  const datetime = formatDate(now);
+  const unixtime = Math.floor(now.getTime() / 1000);
+  const temperature = result.isValid ? result.temperature.toFixed(1) : "[error]";
+  const humidity = result.isValid ? result.humidity.toFixed(1) : "[error]";
 
-  console.log(`${now},${temperature},${humidity}`);
+  return { datetime, unixtime, temperature, humidity };
 }
 
 function formatDate(date: Date): string {
-  const yyyy = date.getFullYear();
-  const mm = String(date.getMonth() + 1).padStart(2, '0');
-  const dd = String(date.getDate()).padStart(2, '0');
-  const hh = String(date.getHours()).padStart(2, '0');
-  const mi = String(date.getMinutes()).padStart(2, '0');
-  const ss = String(date.getSeconds()).padStart(2, '0');
-  return `${yyyy}/${mm}/${dd} ${hh}:${mi}:${ss}`;
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+  const hour = String(date.getHours()).padStart(2, "0");
+  const minute = String(date.getMinutes()).padStart(2, "0");
+  const second = String(date.getSeconds()).padStart(2, "0");
+
+  return `${year}/${month}/${day} ${hour}:${minute}:${second}`;
 }
+
+const PORT = 3000;
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/sensor`);
+});
+
+app.get("/sensor", (_req: Request, res: Response) => {
+  console.log(`[${formatDate(new Date())}] Access from ${_req.ip} to /sensor`);
+
+  try {
+    const data = getSensorData();
+    res.json(data);
+  } catch (e) {
+    res.status(500).json({
+      error: (e as Error).message,
+    });
+  }
+});
